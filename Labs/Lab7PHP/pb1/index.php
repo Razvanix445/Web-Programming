d144e965e0efba118bab98b31417268006ab6c9c
Razvanix445 <razvan.calauz@yahoo.com>
2024-05-28 10:35:31 +0300

    AddedLab7AI

--- Labs/Lab7PHP/pb1/index.php
+++ Labs/Lab7PHP/pb1/index.php
@@ -1,11 +1,3 @@
-<!--În rezolvarea problemei de mai jos se vor efectua toate validările şi verificările necesare pentru a preîntâmpina
-orice problemă de securitate (SQL injection, cross site scripting, CSRF, unrestricted file upload, etc). Nu se va
-folosi JavaScript (sau librarii bazate pe JavaScript). Singurele tehnologii permise pe client sunt HTML si CSS.
-
-1. O baza de date contine trenuri caracterizate de: nr. tren, tip tren, localitate plecare, localitate sosire, ora
-plecare, ora sosire. Un calator va putea cauta trenuri intre doua localitati, specificand prin intermediul unui check
-box daca doreste numai curse directe sau si curse cu legatura (schimbarea trenului intr-o localitate intermediara).-->
-
 <?php
 session_start();
 
